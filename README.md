75cd97d2ca61089ef81764a2d2651742774c2262
BredeFK <bredefk@stud.ntnu.no>
2018-04-13 21:29:16 +0200

    Added proguard-rules, and stated how text to user should be coded

--- README.md
+++ README.md
@@ -7,5 +7,19 @@
 | [Johan Aanesen](https://github.com/johanAanesen/)        | BPROG | xxxxxx |
 | [Jørgen Jærnes](https://github.com/jorgenjj)      | BPROG | xxxxxx |
 
+## Style
+When something is written to the user, it's from resource, so it's easier to change text and/or translate.
+### How
+#### Adding string resource
+[Change the UI strings](https://developer.android.com/training/basics/firstapp/building-ui.html)
+
+### Retrieving string resource
+```java
+  TextView txtHello = findViewById(R.id.hello_ID);
+  // Retrieving the string with id 'hello'  
+  String helloText = this.getResources().getString(R.string.hello);
+  txtHello.setText(helloText);
+```
+
 ## Sources
-- 
+-
--- app/src/main/java/ibelieveicanfly/skydivelogbook/MainActivity.java
+++ app/src/main/java/ibelieveicanfly/skydivelogbook/MainActivity.java
@@ -1,13 +1,40 @@
 package ibelieveicanfly.skydivelogbook;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView txtHello = findViewById(R.id.hello_ID);
+        String helloText = this.getResources().getString(R.string.hello);
+        txtHello.setText(helloText);
+
+        auth = FirebaseAuth.getInstance();
+        userStatus(auth.getCurrentUser());
+    }
+
+    // userStatus checks if user is signed in or not
+    private void userStatus(FirebaseUser currentUser) {
+
+
+        if (currentUser != null) {
+            // User is signed in
+
+            // Code here ...
+        } else {
+            // User is not signed in
+
+            // Code here ...
+        }
     }
 }
